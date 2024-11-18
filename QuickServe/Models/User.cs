090eb720ae6c19f7dae6d2b4a23743dc1a1780be
pvishnuswaroop <pottavishnuswaroop@gmail.com>
2024-11-18 09:30:16 +0530

    changes

--- QuickServe/Models/User.cs
+++ QuickServe/Models/User.cs
@@ -8,27 +8,27 @@ namespace QuickServe.Models
         [Key]
         public int UserID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Name is required.")]
         public string? Name { get; set; }
 
         public string? Gender { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Contact number is required.")]
         public string? ContactNumber { get; set; }
 
-        [Required, EmailAddress]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Address is required.")]
         public string? Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required.")]
         public string? Password { get; set; }  // Hashed password
 
         // Navigation property
         public virtual ICollection<Order> Orders { get; set; } = new List<Order>();  // One-to-many with Order
-        public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();  // One-to-many with Cart
+        public virtual Cart? Cart { get; set; }  // One-to-one with Cart
         public virtual ICollection<Rating> Ratings { get; set; } = new List<Rating>();  // One-to-many with Rating
-
     }
 }
